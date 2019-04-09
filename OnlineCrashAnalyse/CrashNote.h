2627a728c7a1dd06a5b510ccb469b0ab88557fc3
hellogaojun <2293209493@qq.com>
2019-04-09 16:14:42 +0800

    nothing

--- OnlineCrashAnalyse/CrashNote.h
+++ OnlineCrashAnalyse/CrashNote.h
@@ -54,9 +54,6 @@
  4.http://www.cocoachina.com/ios/20180326/22765.html
  5.https://www.jianshu.com/p/cf0945f9c1f8
  
- 关于iOS中常见类型的crash错误的防护机制,可以参考下面这篇文章:
- https://mp.weixin.qq.com/s?__biz=MzUxMzcxMzE5Ng==&mid=2247488311&amp;idx=1&amp;sn=0db090c8d4a5efafa47f00af4b3f174f&source=41&key=136c1b77805d77d5e3fc94f9961e1208e8a1c83f09dd489c62c8423767471a4467f741bbfcd082aa746c107aa7d6d3567ba344fad24ef9d9310961495da22364a024d6bff7ac263ff476828ae770bea4&ascene=0&uin=MTY2Nzc0OTY4MQ%3D%3D&devicetype=iMac+MacBookPro11%2C4+OSX+OSX+10.13.6+build(17G65)&version=12010210&nettype=WIFI&lang=zh_CN&fontScale=100&pass_ticket=PZLiwk2GvlCCMNT7WmjU3s5ly58yCkcbEJvLmX4klJnlEF5y1QmU0ldTSZypp8SM
- 
  */
 
 #endif /* CrashNote_h */
